c5dee01b3c056f1ee944d962f3b5f750cda92101
David Draguta <daviddraguta@gmail.com>
2019-10-26 10:33:50 -0400

    Change paths in defs.sh

--- defs.sh
+++ defs.sh
@@ -1,15 +1,12 @@
 function latex_build {
-    perl ~/.scripts/perl-scripts/latex_build/latex_build.pl $1
+    perl ~/.scripts/latex_build/latex_build.pl $1
 }
 
 function define {
-    perl ~/.scripts/perl-scripts/define/define.pl $1
+    perl ~/.scripts/define/define.pl $1
 }
 
 function perl? {
     perl -MO=Deparse -e $1
 }
 
-function defn {
-    python ~/.scripts/python-scripts/dictionary/dictionary.py $1 $2
-}
