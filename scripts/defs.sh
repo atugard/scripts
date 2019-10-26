079164df7567879ed4acb9ec46bcf621025026e5
David Draguta <daviddraguta@gmail.com>
2019-10-26 10:56:25 -0400

    Apply bold is not bright patch to st.

--- scripts/defs.sh
+++ scripts/defs.sh
@@ -1,12 +1,9 @@
 function latex_build {
-    perl ~/.scripts/latex_build/latex_build.pl $1
+    perl ~/tools/scripts/latex_build/latex_build.pl $1
 }
 
 function define {
-    perl ~/.scripts/define/define.pl $1
+    perl ~/tools/scripts/define/define.pl $1
 }
 
-function perl? {
-    perl -MO=Deparse -e $1
-}
 
