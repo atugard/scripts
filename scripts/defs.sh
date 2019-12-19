918addf21c05ff23fbaa6f6f3f18923ac03fe3f4
David Draguta <daviddraguta@gmail.com>
2019-12-18 21:35:01 -0500

    Added a check to see if I REALLY want to shut down my computer, since I've accidently pressed s and shut it down like 923893894 times already X.X

--- scripts/defs.sh
+++ scripts/defs.sh
@@ -6,4 +6,19 @@ function define {
     perl ~/tools/scripts/define/define.pl $1
 }
 
+function u {
+    sudo pacman -Syu
+}
+
+function s {
+    echo "Are you sure that you want to shut down your computer?"
+    read answer
+    if [ "$answer" = "yes" ] || [ "$answer" = "y" ]; then
+        shutdown -now
+    fi
+    if [ "$answer" = "no" ] || [ "$answer" = "n" ]; then
+        echo "Saved your ass. HEEEEEEEEEEEEEEEEEEEEE."
+    fi
+}
+
 
