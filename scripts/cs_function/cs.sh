c1039d3bc526bb604f59c2adf2b460b0fb138a7d
David Draguta <daviddraguta@gmail.com>
2020-01-05 14:54:19 -0500

    Changed ch1 handling of cs_function to include option to upload to git. I do that way too often to not automate at least part of it.

--- scripts/cs_function/cs.sh
+++ scripts/cs_function/cs.sh
@@ -3,7 +3,13 @@ function cs {
     read chapter
     #handle chapter 1
     if [ $chapter = 1 ]; then
-        echo "Would you like to open notes_and_exercises in vim or load it in scheme? (vim/scheme): \n"
+        echo "Would you like to \n
+        1. (vim) Open notes_and_exercises in vim?  \n
+        2. (scheme) Load notes_and_exercises in scheme? \n
+        3. (git) Add, commit, and push notes_and_exercises to git repo? \n
+
+        Type vim, scheme, or git, or anything else to quit: "
+              
         read answer
         if [ "$answer" = "vim" ]; then
             vim ~/Dropbox/computer_science/$chapter/notes_and_exercises.scm
@@ -11,6 +17,15 @@ function cs {
         if [ "$answer" =  "scheme" ]; then
             scheme --load ~/Dropbox/computer_science/$chapter/notes_and_exercises.scm
         fi
+        if [ "$answer" = "git" ]; then
+            echo "What is your commit message?"
+            read commit_message
+            cd ~/Dropbox/computer_science/1
+            git add ~/Dropbox/computer_science/1/notes_and_exercises.scm
+            git commit -m "$commit_message"
+            git push origin master
+        fi
+            
     fi
     #handle chapter 2
     if [ $chapter = 2 ]; then
