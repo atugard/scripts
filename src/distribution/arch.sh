0418f92a08e6ec25b3bec8207f45bc4d0d258ec6
David Draguta <daviddraguta@gmail.com>
2020-11-19 07:39:19 -0500

    Add two optional arguments to specify location of mirrors.

--- src/distribution/arch.sh
+++ src/distribution/arch.sh
@@ -1,6 +1,6 @@
 mirrors(){
   echo "Fetching mirrors..." &&
-    curl -s "https://www.archlinux.org/mirrorlist/?country=CA&country=US&protocol=https&use_mirror_status=on" | 
+    curl -s "https://www.archlinux.org/mirrorlist/?country=${2:-CA}&country=${3:-US}&protocol=https&use_mirror_status=on" | 
     sed -e 's/^#Server/Server/' -e '/^#/d' | 
     rankmirrors -n ${1:-5} - > ~/mirrors &&
     sudo cp ~/mirrors /etc/pacman.d/mirrorlist &&
