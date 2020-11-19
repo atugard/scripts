99847e0dd0a3dbbbf039f492d7e2311f82f44661
David Draguta <daviddraguta@gmail.com>
2020-11-19 09:12:19 -0500

    Use /tmp directory rather than home.

--- src/distribution/arch.sh
+++ src/distribution/arch.sh
@@ -2,8 +2,8 @@ mirrors(){
   echo "Fetching mirrors..." &&
     curl -s "https://www.archlinux.org/mirrorlist/?country=${2:-CA}&country=${3:-US}&protocol=https&use_mirror_status=on" | 
     sed -e 's/^#Server/Server/' -e '/^#/d' | 
-    rankmirrors -n ${1:-5} - > ~/mirrors &&
-    sudo cp ~/mirrors /etc/pacman.d/mirrorlist &&
-    rm ~/mirrors &&
+    rankmirrors -n ${1:-5} - > /tmp/mirrors &&
+    sudo cp /tmp/mirrors /etc/pacman.d/mirrorlist &&
+    rm /tmp/mirrors &&
     echo "Done!"
   }
