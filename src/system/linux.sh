25a96b9c5e7a9123d9a6a11911051c2db96ffacf
David Draguta <daviddraguta@gmail.com>
2020-12-20 09:44:14 -0500

    Remove no argument case.

--- src/system/linux.sh
+++ src/system/linux.sh
@@ -5,14 +5,12 @@ umnt(){
   sudo umount ~/Drives/usb
 }
 search(){
-  if [[ $# == 0 ]]; then
-    doas find /
-  elif [[ $# == 1 ]]; then
+  if [[ $# == 1 ]]; then
     doas find / -name "*$1*" 
   elif [[ $# == 2 ]]; then 
     search $1 | grep $2 
   else
-    echo "search takes either 0,1, or 2  arguments. You gave $#."
+    echo "search takes either 1, or 2  arguments. You gave $#."
   fi
 }
 s(){
